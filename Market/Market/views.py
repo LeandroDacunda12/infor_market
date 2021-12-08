d2297bd2619057f1b4fd6a3993409cdcc3b22672
LeandroDacunda12 <95589672+LeandroDacunda12@users.noreply.github.com>
2021-12-08 11:25:02 -0600

    comercio

--- Market/Market/views.py
+++ Market/Market/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 
 def inicio(request):
+	usuario = {
+		"nombre": "Leandro",
+		"apellido": "Dacunda"
+	}
+	productos = [
+		{"nombre" : "computadora","precio": 12},
+		{"nombre": "teclado", "precio": 120},
+
+	]
+
+	contex ={
+		"usuario": usuario,
+		"productos": productos
+	}
+
 	return render(request, "inicio.html")
