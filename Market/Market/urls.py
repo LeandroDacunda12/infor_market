7dd8cc66ce12d8fc16f26fa40d6104ab13568264
LeandroDacunda12 <95589672+LeandroDacunda12@users.noreply.github.com>
2021-12-12 19:33:37 -0600

    cree admin y login

--- Market/Market/urls.py
+++ Market/Market/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/',views.inicio, name="inicio"),
-    path('login/',views.login),
+    path('',views.inicio, name="inicio"),
+    path('login/',auth_views.LoginView.as_view(template_name='login.html'), name="login"),
 ]
